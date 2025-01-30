1f7934d91e368caf13cccfa5c785cf5e68d8db59
NaveenKumar Raja <rnaveenk666@gmail.com>
2025-01-30 20:14:06 +0530

    changed header scroll to fixed in top

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         document.getElementById('welcome-message').classList.add('hidden');
         document.getElementById('main-content').classList.remove('hidden');
-    }, 3000); // Change the duration (3000ms = 3 seconds) as needed
+    }, 10000); // Change the duration (3000ms = 3 seconds) as needed
 });
 
+const header = document.getElementById('header');
+let lastScrollY = window.scrollY;
 
+window.addEventListener('scroll', () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY) {
+        // Scrolling down
+        header.classList.add('fixed-header');
+    } else {
+        // Scrolling up
+        header.classList.remove('fixed-header');
+    }
+
+    lastScrollY = currentScrollY;
+});
